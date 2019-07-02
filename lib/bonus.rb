e1d4110f1e708f8c3267c3723e9b8cda62fcb194
Aida Abdikulova <abdikulova@gmail.com>
2019-07-02 03:25:30 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,6 +23,7 @@ def bonus
   #code your solution here:
 epic_tragedy[:capulet][:heroine][:name][:age][:status]="dead"
 epic_tragedy[:montague][:hero][:name][:age][:status]="dead"
+puts epic_tragedy
 
 
 
